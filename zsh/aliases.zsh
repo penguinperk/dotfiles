52991386fc38dd5c2d35dda3dad341ca0c1f55a4
Scott Perkins <perk@penguinnorth.com>
2025-08-25 12:11:45 -0500

    adding azCLI

---
+++ zsh/aliases.zsh
@@ -0,0 +1,22 @@
+# Alias
+# ---
+#
+alias k="kubectl"
+alias h="helm"
+alias tf="terraform"
+alias a="ansible"
+alias ap="ansible-playbook"
+
+alias code="open -a 'Visual Studio Code'"
+
+alias gprj="cd $HOME/Projects"
+alias gcs="cd $HOME/Projects/christianlempa/cheat-sheets"
+alias gbp="cd $HOME/Projects/christianlempa/boilerplates"
+
+#alias ls="exa --icons --group-directories-first"
+#alias ll="exa --icons --group-directories-first -l"
+alias grep='grep --color'
+
+alias prx="ssh root@prx-prod-2.home.clcreative.de"
+
+alias azll="az login --tenant lrsmanaged.com"
