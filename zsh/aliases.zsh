53e42de09bd98bc10c578408f64492efed9309a9
Scott Perkins <perk@penguinnorth.com>
2025-08-28 14:53:03 -0500

    adding azure alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,10 @@
 # Alias
 # ---
 #
+#
+alias visource='vi ~/.zshrc'
+alias sourceme='source ~/.zshrc'
+
 alias k="kubectl"
 alias h="helm"
 alias tf="terraform"
@@ -8,6 +12,7 @@ alias a="ansible"
 alias ap="ansible-playbook"
 
 alias code="open -a 'Visual Studio Code'"
+alias cursor="open -a 'cursor'"
 
 alias gprj="cd $HOME/Projects"
 alias gcs="cd $HOME/Projects/christianlempa/cheat-sheets"
@@ -20,3 +25,8 @@ alias grep='grep --color'
 alias prx="ssh root@prx-prod-2.home.clcreative.de"
 
 alias azll="az login --tenant lrsmanaged.com"
+alias azlsub='az account list --query "[].{Name:name, Id:id, IsDefault:isDefault, State:state, Tenant:tenantId}" -o table'
+alias azlvm='az vm list -d --query "[].{Name:name, ResourceGroup:resourceGroup, Location:location, State:powerState}" -o table'
+alias azsetsub='az account set --subscription $1'
+alias azlrg='az group list --output table'
+alias azgetsub='az account show --query "{Name:name, SubscriptionId:id, IsDefault:isDefault}" -o table'
